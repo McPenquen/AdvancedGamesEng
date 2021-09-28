8e21e2447710bb1d505f9561b30fa667398a9170
McPenquen <majda.calk@outlook.com>
2021-09-28 16:40:28 +0200

    Add camera rotation

--- Assets/Scripts/InputMovement.cs
+++ Assets/Scripts/InputMovement.cs
@@ -7,24 +7,35 @@ public class InputMovement : MonoBehaviour
 {
     [Header("Movement Variables")]
 
-    [SerializeField] private int speed = 10;
+
+    [SerializeField] private int movementSpeed = 10;
+    [SerializeField] private int rotationSpeed = 5;
 
     void Update()
     {
         // Detect input
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        float mouseXInput = Input.GetAxis("Mouse X");
+        float mouseYInput = Input.GetAxis("Mouse Y");
 
-        // If we detect an input lets move
+        // If we detect a motion input lets move
         if (horizontalInput != 0 || verticalInput != 0)
         {
             // Get the new position
             Vector3 newPosition = transform.position  
-                + transform.forward * speed * Time.deltaTime * verticalInput
-                + - Vector3.Cross(transform.forward, transform.up) * speed * Time.deltaTime * horizontalInput;
+                + transform.forward * movementSpeed * Time.deltaTime * verticalInput
+                + - Vector3.Cross(transform.forward, transform.up) * movementSpeed * Time.deltaTime * horizontalInput;
 
             // Move the object to the new position
             transform.position = newPosition;
         }
+
+        // If we detect mouse input rotate
+        if (mouseXInput != 0 || mouseYInput != 0)
+        {
+            transform.Rotate(-mouseYInput * rotationSpeed, mouseXInput * rotationSpeed, 0);
+        }
+
     }
 }
