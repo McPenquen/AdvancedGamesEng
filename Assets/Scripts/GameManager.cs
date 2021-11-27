b1d5321e9824fda3a308de393a2c48530da38562
McPenquen <majda.calk@outlook.com>
2021-11-27 17:06:10 +0100

    Add scene navigation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    // Null instance of the manager
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        // SINGLETON
+        // Check if instance is null
+        if (instance == null)
+        {
+            //Don't destroy the current game manager
+            DontDestroyOnLoad(gameObject);
+
+            //Set game manager instance to this
+            instance = this;
+        }
+        // Check if current instance of game manager is equal to this game manager
+        else if (instance != this)
+        {
+            //Destroy the game manager that is not the current game manager
+            Destroy(gameObject);
+        }
+    }
     void Update()
     {
         // ESC press quits the game
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
+        }
+        // Else navigate between scenes by numbers 1-5 -> scene switch
+        else if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("DevelopmentScene");
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("MultipleLightSourcesScene");
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("ComplexObjectsScene");
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene("GlassObjectsScene");
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad5) || Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SceneManager.LoadScene("ColouredGlassScene");
         }
     }
 }
